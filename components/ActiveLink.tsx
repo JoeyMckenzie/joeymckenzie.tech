32d5f7133d346487c17341bce5603f3a28e1a389
Joey Mckenzie <joey.mckenzie27@gmail.com>
2022-02-04 12:42:16 -0800

    feat(components): add active link

---
+++ components/ActiveLink.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { FC } from 'react';
+import { classNames } from '@lib';
+
+interface ActiveLinkProps {
+  className: string;
+  activeClassName: string;
+  defaultClassName: string;
+  href: string;
+}
+
+export const ActiveLink: FC<ActiveLinkProps> = ({
+  children,
+  className,
+  activeClassName,
+  defaultClassName,
+  href,
+}) => {
+  const { pathname } = useRouter();
+
+  const activeClass = (href: string) =>
+    pathname === href ? activeClassName : defaultClassName;
+
+  return (
+    <Link href={href} passHref>
+      <a className={classNames(activeClass(href), className)}>{children}</a>
+    </Link>
+  );
+};
+
+ActiveLink.propTypes = {
+  activeClassName: PropTypes.string.isRequired,
+};
