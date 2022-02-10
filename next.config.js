7b82ee1758f857d2e14795f4125fc52ff94623bb
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-02-09 18:35:00 -0800

    feat(workspace): add docker support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+  },
   images: {
     // Add your custom domains here
     domains: ['tailwindui.com', 'images.unsplash.com'],
