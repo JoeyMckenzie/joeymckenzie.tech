34b9caee20335058afab5bc77e6019468c3956df
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-07-09 12:18:47 -0700

    chore(intro): remove if on snippet page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { VFC } from 'react';
+import { useCallback, VFC } from 'react';
 import Navbar from '@/components/Navbar';
 import BlogSearchContextProvider from '@/lib/contexts/blog-search.context';
 import AlertContextProvider from '@/lib/contexts/alert.context';
@@ -15,7 +15,11 @@ import { useRouter } from 'next/router';
 const CustomApp: VFC<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
 
-  const isBlogPage = router.pathname.indexOf('/blog/') > -1;
+  const isPathRoute = (routeFragment: string) =>
+    router.pathname.indexOf(routeFragment) > -1;
+
+  const isSnippetOrBlogPage =
+    isPathRoute('/blog/') || isPathRoute('/snippets/');
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
@@ -37,7 +41,7 @@ const CustomApp: VFC<AppProps> = ({ Component, pageProps }) => {
           <Modal />
           <Notification />
           <Navbar />
-          {!isBlogPage && <Intro />}
+          {!isSnippetOrBlogPage && <Intro />}
           <Component {...pageProps} />
           <FooterWithLinks />
         </AlertContextProvider>
