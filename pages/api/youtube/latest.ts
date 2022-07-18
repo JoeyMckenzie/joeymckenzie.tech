ff0de9d28a81386f86f4ddb0ee9c924e24c57720
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-07-17 21:55:15 -0700

    feat(services): add youtube api

--- pages/api/youtube/latest.ts
+++ pages/api/youtube/latest.ts
@@ -1,6 +1,6 @@
-import { LatestVideosResponse } from '@/lib/types/youtube.types';
 import { NextApiRequest, NextApiResponse } from 'next';
-import playlist from './playlist.json';
+import { google } from 'googleapis';
+import { catchError, firstValueFrom, from, map, of, tap } from 'rxjs';
 
 export default function handler(
   request: NextApiRequest,
@@ -10,7 +10,32 @@ export default function handler(
     return response.status(405);
   }
 
-  return response.status(200).json({
-    videos: playlist.videos,
-  } as LatestVideosResponse);
+  const client = google.youtube({
+    version: 'v3',
+  });
+
+  const personalChannels$ = from(
+    client.channels.list({
+      part: ['contentDetails'],
+      forUsername: 'JoeyMcKenzie',
+      key: process.env.YOUTUBE_API_KEY,
+    })
+  ).pipe(
+    tap((response) => console.log(response.data)),
+    map((channels) =>
+      response.status(200).json({
+        channels,
+      })
+    ),
+    catchError((error) => {
+      console.error(error);
+      return of(
+        response.status(500).json({
+          message: error.toString(),
+        })
+      );
+    })
+  );
+
+  return firstValueFrom(personalChannels$);
 }
