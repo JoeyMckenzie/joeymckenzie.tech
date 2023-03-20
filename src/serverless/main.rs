8f7616f709c474f7b3f3d1894fc1429e3e88828c
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-03-20 15:59:56 -0700

    chore: update shuttle

--- src/serverless/main.rs
+++ src/serverless/main.rs
@@ -6,13 +6,11 @@ mod spotify;
 use router::{build_router, build_spotify_client_from_shuttle_secrets};
 use shuttle_secrets::SecretStore;
 
-use sync_wrapper::SyncWrapper;
-
 /// The shuttle entry point injected with our secrets store at dev/runtime with secrets from `Secrets.toml`. Don't check that file in!
-#[shuttle_service::main]
+#[shuttle_runtime::main]
 async fn spotify(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
-) -> shuttle_service::ShuttleAxum {
+) -> shuttle_axum::ShuttleAxum {
     let spotify_client = build_spotify_client_from_shuttle_secrets(&secret_store);
     let timeout_duration_seconds = secret_store
         .get("TIMEOUT_DURATION_SECONDS")
@@ -21,7 +19,6 @@ async fn spotify(
         .expect("timeout duration is not valid");
 
     let router = build_router(timeout_duration_seconds, spotify_client);
-    let sync_wrapper = SyncWrapper::new(router);
 
-    Ok(sync_wrapper)
+    Ok(router.into())
 }
