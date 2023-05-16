275dcd732ebe7fe1265f4d390face28dad8028ab
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-05-16 00:39:02 -0700

    chore: update footer spacing

--- src/components/footer/spotify/SpotifyNotCurrentlyListening.tsx
+++ src/components/footer/spotify/SpotifyNotCurrentlyListening.tsx
@@ -8,7 +8,7 @@ const SpotifyNotCurrentlyListening: Component<
   ParentProps<NotCurrentlyListeningProps>
 > = (props) => (
   <div class="flex flex-col space-y-1 pt-6 md:pt-0">
-    <div class="flex flex-row items-center justify-center space-x-2">
+    <div class="flex flex-row items-center justify-center gap-x-2">
       {props.children}
       <div class="flex flex-col">
         <h4 class="text-xs text-neutral-500">
