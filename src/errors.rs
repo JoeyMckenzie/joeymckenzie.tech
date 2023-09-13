040bba4f37576ced87a51892b0a5e012c43deab0
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-09-12 23:26:13 -0700

    chore: update routes

--- src/errors.rs
+++ src/errors.rs
@@ -1,16 +1,29 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::response::{IntoResponse, Response};
 use thiserror::Error;
 
-#[derive(Error, PartialEq, Eq, Debug)]
+use crate::routes;
+
+#[derive(Error, Debug)]
 pub enum AppError {
     #[error("An unexpected error occurred.")]
     InternalError,
+    #[error("An error occurred reading an environment variable.")]
+    EnvVarUnreadable(#[from] std::env::VarError),
+    #[error("An error occurred attempting to render a template.")]
+    TemplateRenderError(#[from] tera::Error),
+    #[error("Blog page was not found.")]
+    BlogPageNotFound,
 }
 
 impl IntoResponse for AppError {
-    fn into_response(self) -> axum::response::Response {
-        match self {
-            AppError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
-        }
+    fn into_response(self) -> Response {
+        let error = match self {
+            AppError::InternalError => routes::get_internal_error_page(),
+            AppError::EnvVarUnreadable(_) => routes::get_internal_error_page(),
+            AppError::TemplateRenderError(_) => todo!(),
+            AppError::BlogPageNotFound => todo!(),
+        };
+
+        error.into_response()
     }
 }
--- src/routes.rs
+++ src/routes.rs
@@ -5,17 +5,20 @@ use axum::{
 };
 use tera::Context as TeraContext;
 
-use crate::cache::{BLOG_CONTENT_CACHE, TEMPLATE_CACHE};
+use crate::{
+    cache::{BLOG_CONTENT_CACHE, TEMPLATE_CACHE},
+    errors::AppError,
+};
 
-pub async fn home() -> impl IntoResponse {
-    let url = std::env::var("BASE_URL")?;
-    let mut context = tera::Context::new();
-    context.insert("title", "Hi, I'm Joe.");
-    context.insert("description", "A guy that likes beer and code.");
-    context.insert("canonicalURL", &url);
-    context.insert("openGraphURL", &url);
-    context.insert("openGraphImage", &url);
-    context.insert("twitterImage", &url);
+type PageResult = Result<Html<String>, AppError>;
+
+pub async fn home() -> PageResult {
+    let context = get_tera_context("Hi, I'm Joe.", "A guy that likes beer and code.")?;
+
+    if let Some(content_cache) = TEMPLATE_CACHE.get() {
+        let content = content_cache.render("pages/home.html", &context)?;
+        return Ok(Html(content));
+    }
 
     let content = TEMPLATE_CACHE
         .get()
@@ -23,7 +26,7 @@ pub async fn home() -> impl IntoResponse {
         .render("pages/home.html", &context)
         .unwrap();
 
-    Html(content)
+    Ok(Html(content))
 }
 
 pub async fn blogs() -> impl IntoResponse {
@@ -36,31 +39,46 @@ pub async fn blogs() -> impl IntoResponse {
     Html(content)
 }
 
-pub async fn blog_page(Path(slug): Path<String>) -> (StatusCode, Html<String>) {
+pub async fn blog_page(Path(slug): Path<String>) -> PageResult {
     // Get the content from cache, where the cache key is the slug and the value is the bundled HTML with content response
     let blog_content = BLOG_CONTENT_CACHE.get().unwrap().get(&slug);
 
     match blog_content {
-        Some(content) => (StatusCode::OK, content.to_owned()),
-        None => not_found().await,
+        Some(content) => Ok(content.to_owned()),
+        None => Err(AppError::BlogPageNotFound),
     }
 }
 
-pub async fn not_found() -> (StatusCode, Html<String>) {
-    let url = std::env::var("BASE_URL").unwrap();
+pub async fn not_found() -> PageResult {
+    let context = get_tera_context("Gone fishing.", "A guy that likes beer and code.")?;
+
+    if let Some(tera) = TEMPLATE_CACHE.get() {
+        let rendered_template = tera.render("pages/not-found.html", &context)?;
+        return Ok(Html(rendered_template));
+    }
+
+    match TEMPLATE_CACHE.get() {
+        Some(tera) => {
+            let rendered_template = tera.render("pages/not-found.html", &context)?;
+            Ok(Html(rendered_template))
+        }
+        None => Err(AppError::InternalError),
+    }
+}
+
+pub fn get_internal_error_page() -> impl IntoResponse {
+    let context = tera::Context::new();
+    (StatusCode::INTERNAL_SERVER_ERROR, Html("".to_string()))
+}
+
+pub fn get_tera_context(title: &str, description: &str) -> Result<tera::Context, AppError> {
+    let url = std::env::var("BASE_URL")?;
     let mut context = tera::Context::new();
-    context.insert("title", "Gone fishing.");
-    context.insert("description", "A guy that likes beer and code.");
+    context.insert("title", title);
+    context.insert("description", description);
     context.insert("canonicalURL", &url);
     context.insert("openGraphURL", &url);
     context.insert("openGraphImage", &url);
     context.insert("twitterImage", &url);
-
-    let not_found_page = TEMPLATE_CACHE
-        .get()
-        .unwrap()
-        .render("pages/not-found.html", &context)
-        .unwrap();
-
-    (StatusCode::NOT_FOUND, Html(not_found_page))
+    Ok(context)
 }
