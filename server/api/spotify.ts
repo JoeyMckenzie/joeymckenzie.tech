858c4553a7db4a15f4263fd8a35afe1b1500dee8
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-09-22 17:55:57 -0700

    chore: code cleanup

--- server/api/spotify.ts
+++ server/api/spotify.ts
@@ -2,34 +2,32 @@
 
 type AccessTokenResponse = {
   access_token: string;
+  token_type: string;
 };
 
 export default defineEventHandler(async () => {
   const config = useRuntimeConfig();
-  const { access_token: accessToken } = await $fetch<AccessTokenResponse>(
-    config.app.spotifyTokenEndpoint,
-    {
+  const { access_token: accessToken, token_type: tokenType } =
+    await $fetch<AccessTokenResponse>(config.app.spotifyTokenEndpoint, {
       method: 'POST',
       headers: getRequestHeaders(
         config.app.spotifyClientId,
         config.app.spotifyClientSecret,
       ),
       body: getRequestBody(config.app.spotifyRefreshToken),
-    },
-  );
+    });
 
-  console.log(accessToken);
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const nowPlayingResponse = await $fetch(
     config.app.spotifyNowPlayingEndpoint,
     {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `${tokenType} ${accessToken}`,
       },
     },
   );
+
+  console.log(nowPlayingResponse);
 
   return {
     foo: 'bar',
