124600832a991bf7640de36bd23c14db29fda607
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-09-24 10:45:31 -0700

    chore: use fallback value

--- server/drizzle/db.ts
+++ server/drizzle/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 neonConfig.fetchConnectionCache = true;
 
-const sql = neon(import.meta.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL ?? '');
 const db = drizzle(sql, { logger: true });
 
 export default db;
