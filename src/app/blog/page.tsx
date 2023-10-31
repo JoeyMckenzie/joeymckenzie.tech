e41597a309ca1cbcad5be9c690395a6b9ac24b89
Joey <joey.mckenzie27@gmail.com>
2023-10-30 17:08:10 -0700

    chore: spacing

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -16,8 +16,9 @@ export default function Blog() {
           software design. I consider my writing a journal of technologies
           I&apos;ve worked with at some point during my career, and I&apos;m
           always happy to field questions and conversations from interested
-          readers. Feel free to contact me about any of the writing I do here,
-          or to simply say hello!
+          readers. Feel free to{' '}
+          <a href="mailto:joey.mckenzie27@gmail.com">contact</a> me about any of
+          the writing I do here, or to simply say hello!
         </p>
       </div>
       <BlogPreview posts={posts} />
