c9bfa695a1bfc00cd6d92decaae0f6466d92745e
Joey <joey.mckenzie27@gmail.com>
2023-11-01 17:48:08 -0700

    chore: content updates

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,8 +3,8 @@ import rehypePrettyCode from 'rehype-pretty-code';
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/*.mdx`,
-  contentType: 'mdx',
+  filePathPattern: `**/*.md`,
+  contentType: 'markdown',
   fields: {
     title: { type: 'string', required: true },
     description: { type: 'string', required: true },
