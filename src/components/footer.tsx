1b0e344d8d5d68353e6c7b077d45ea02f7a1d8f6
Joey <joey.mckenzie27@gmail.com>
2023-11-02 12:53:57 -0700

    chore: remove spotify components

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,13 @@
-import { BsSpotify } from 'react-icons/bs';
 import { PoweredBy } from './powered-by';
 import { SocialIcons } from './socials-icons';
-import { NowPlaying } from './spotify';
 
 export function Footer() {
   return (
     <div className="mx-auto flex w-full max-w-screen-lg flex-col items-center justify-evenly gap-y-8 p-8 sm:flex-row sm:items-end">
       <PoweredBy />
-      <NowPlaying>
+      {/* <NowPlaying>
         <BsSpotify className="h-6 w-6 text-green-500" />
-      </NowPlaying>
+      </NowPlaying> */}
       <SocialIcons />
     </div>
   );
