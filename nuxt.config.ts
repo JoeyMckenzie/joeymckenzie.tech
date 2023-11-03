104d2aa7e83fe1e43dcc5fcefd3bb15e564bfb98
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-11-02 21:57:02 -0700

    chore: back to netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'netlify',
+    preset: 'vercel',
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
