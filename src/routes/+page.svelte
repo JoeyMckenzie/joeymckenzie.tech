1bbe03c0b04da1c3881212482278fac48d8f717b
Joey <joey.mckenzie27@gmail.com>
2023-11-03 16:00:56 -0700

    chore: add toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+  import Toggle from '$lib/components/ui/toggle/toggle.svelte';
+</script>
+
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
+<Toggle>b</Toggle>
--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from 'tailwindcss';
-
-export default {
-  content: ['./src/**/*.{html,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
