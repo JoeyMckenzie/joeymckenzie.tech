24a19dbf834501157028d323f8439a184b174093
Joey <joey.mckenzie27@gmail.com>
2023-11-03 16:02:08 -0700

    chore: add button?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import Button from '$lib/components/ui/button/button.svelte';
   import Toggle from '$lib/components/ui/toggle/toggle.svelte';
 </script>
 
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
 <Toggle>b</Toggle>
+<Button>button</Button>
