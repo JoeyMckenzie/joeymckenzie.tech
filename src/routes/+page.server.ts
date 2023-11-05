eb146452132c6e3628f078c70b9c92038c4cb1af
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-11-05 12:38:26 -0800

    chore: add content cache for blog previews

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,9 @@
 import type { PageServerLoad } from './$types';
 import { loadPostPreviews } from '$lib/loaders';
 
-export const load: PageServerLoad = () => loadPostPreviews(true);
+export const load: PageServerLoad = ({ setHeaders }) => {
+  setHeaders({
+    'Cache-Control': 'max-age=604800',
+  });
+  return loadPostPreviews(true);
+};
