54a9d36141dc24df9101f3ef1eec4dd5b505267a
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-11-05 10:14:00 -0800

    chore: add vercel env vars

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,10 @@
-import { VERCEL_COMMIT_REF } from '$env/static/private';
+import { VERCEL_GIT_COMMIT_SHA } from '$env/static/private';
 import type { LayoutServerLoad } from './$types';
 import { getSpotifyNowPlaying } from '$lib/spotify';
 
 export const load: LayoutServerLoad = async () => {
   return {
-    commitSha: VERCEL_COMMIT_REF,
+    commitSha: VERCEL_GIT_COMMIT_SHA,
     nowPlaying: await getSpotifyNowPlaying(),
   };
 };
