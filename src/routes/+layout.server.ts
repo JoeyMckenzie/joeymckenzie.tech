4b2eeb80148db7ceb4a5b58b26ba13c0cfb4ae6b
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-11-05 09:58:38 -0800

    chore: move to vercel

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,10 @@
-import { CF_PAGES_COMMIT_SHA } from '$env/static/private';
+import { VERCEL_COMMIT_REF } from '$env/static/private';
 import type { LayoutServerLoad } from './$types';
 import { getSpotifyNowPlaying } from '$lib/spotify';
 
 export const load: LayoutServerLoad = async () => {
   return {
-    commitSha: CF_PAGES_COMMIT_SHA,
+    commitSha: VERCEL_COMMIT_REF,
     nowPlaying: await getSpotifyNowPlaying(),
   };
 };
