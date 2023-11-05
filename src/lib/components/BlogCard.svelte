00b6a9763b6a64ad7ef6d66bcbea27598c0c3b03
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-11-05 12:33:03 -0800

    chore: move preload to tap

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -7,25 +7,25 @@
   export let post: PostWithViewCount;
 </script>
 
-<article
-  class="hover:scale-102 flex max-w-xl flex-col items-start transition duration-150 ease-in-out hover:-translate-y-1"
->
-  <div class="flex items-center gap-x-4 text-xs">
-    <time dateTime={post.pubDate}>
-      {format(new Date(post.pubDate), 'PP')}
-    </time>
-    <Badge>{post.category}</Badge>
-    <div class="font-medium text-neutral-400">{post.viewCount} views</div>
-  </div>
-  <div class="group relative">
-    <h3 class="mt-3 text-lg font-semibold leading-6">
-      <a href={post.url}>
+<a data-sveltekit-preload-data="tap" href={post.url}>
+  <article
+    class="hover:scale-102 flex max-w-xl flex-col items-start transition duration-150 ease-in-out hover:-translate-y-1"
+  >
+    <div class="flex items-center gap-x-4 text-xs">
+      <time dateTime={post.pubDate}>
+        {format(new Date(post.pubDate), 'PP')}
+      </time>
+      <Badge>{post.category}</Badge>
+      <div class="font-medium text-neutral-400">{post.viewCount} views</div>
+    </div>
+    <div class="group relative">
+      <h3 class="mt-3 text-lg font-semibold leading-6">
         <span class="absolute inset-0" />
         {post.title}
-      </a>
-    </h3>
-    <p class="mt-5 line-clamp-3 text-sm leading-6">
-      {post.description}
-    </p>
-  </div>
-</article>
+      </h3>
+      <p class="mt-5 line-clamp-3 text-sm leading-6">
+        {post.description}
+      </p>
+    </div>
+  </article>
+</a>
