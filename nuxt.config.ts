ce8d6fbaa347313c66239a8b0f002b3320a00a54
Joey <joey.mckenzie27@gmail.com>
2023-11-14 12:15:09 -0800

    poc: move to ssg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
   srcDir: 'src',
   nitro: {
     preset: 'vercel',
