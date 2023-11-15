a0ac43185df692a582105169e49f1f5184bec489
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-11-14 20:47:34 -0800

    chore: wip

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { Head } from '@inertiajs/vue3';
+</script>
+<template>
+    <Head>
+        <title>Bears. Beets. Battlestar Galactiga. | joeymckenzie.tech</title>
+        <meta
+            content="A blog about software, technology, and sometimes beer."
+            name="description"
+        />
+        <meta content="software, programming, technology" name="keywords" />
+    </Head>
+
+    <h2 class="text-4xl font-bold tracking-tight sm:text-center">
+        Hi, I'm Joey.
+    </h2>
+    <p class="prose dark:prose-invert mx-auto mt-6 text-justify leading-6">
+        I'm a <span class="font-semibold">Senior Software Engineer</span> based
+        in Northern California working in fintech. I enjoy writing about
+        software, design, dad jokes, and cheap beer among a few other things. I
+        like building fast, efficient web services, learning new things, and
+        writing code in the open source ecosystem.
+    </p>
+    <!-- <SocialButtons /> -->
+    <h2
+        class="pb-4 pt-8 text-right text-4xl font-bold tracking-tight sm:text-center"
+    >
+        Latest thoughts.
+    </h2>
+    <!-- <BlogPreview posts={$viewCountStore.latest} /> -->
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,13 +16,17 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Home');
 });
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
