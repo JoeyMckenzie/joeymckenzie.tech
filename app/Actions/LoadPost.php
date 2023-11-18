505e7ae70ee2f54419f967c49aa72d3e3f804b3f
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-11-18 14:09:38 -0800

    chore: wip

---
+++ app/Actions/LoadPost.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions;
+
+final readonly class LoadPost
+{
+    public function __invoke(): void
+    {
+        echo 'doghouse';
+    }
+}
---
+++ app/Models/FrontMatter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+final readonly class FrontMatter
+{
+    public string $title;
+
+    public string $description;
+
+    public string $pubDate;
+
+    public string $heroImage;
+
+    public string $category;
+
+    /**
+     * @var array<string>
+     */
+    public array $keywords;
+
+    public function __construct(mixed $frontMatter)
+    {
+        $this->title = $frontMatter['title'];
+        $this->description = $frontMatter['description'];
+        $this->pubDate = $frontMatter['pubDate'];
+        $this->heroImage = $frontMatter['heroImage'];
+        $this->category = $frontMatter['category'];
+        $this->keywords = $frontMatter['keywords'];
+    }
+}
