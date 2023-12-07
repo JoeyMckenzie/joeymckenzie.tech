129728edd0f2eb84d914dbf7c854f320859a5ed2
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-12-07 14:10:54 -0800

    chore: wip

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,10 +28,6 @@ class RouteServiceProvider extends ServiceProvider
             ->by($request->user()?->id ?? $request->ip()));
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
