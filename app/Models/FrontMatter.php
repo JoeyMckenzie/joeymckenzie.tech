f9967bf8ea58323e77648cfae2d1cc314a730049
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-12-07 13:43:02 -0800

    chore: wip

--- app/Models/FrontMatter.php
+++ app/Models/FrontMatter.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Exception;
+
 final readonly class FrontMatter
 {
     public string $title;
@@ -19,13 +21,20 @@ final readonly class FrontMatter
     /** @var string[] */
     public array $keywords;
 
+    /**
+     * @throws Exception
+     */
     public function __construct(mixed $frontMatter)
     {
-        $this->title = $frontMatter['title'];
-        $this->description = $frontMatter['description'];
-        $this->pubDate = $frontMatter['pubDate'];
-        $this->heroImage = $frontMatter['heroImage'];
-        $this->category = $frontMatter['category'];
-        $this->keywords = $frontMatter['keywords'];
+        if (! is_array($frontMatter)) {
+            throw new Exception('Frontmatter structure is not an expected array');
+        }
+
+        $this->title = $frontMatter['title'] ?? '';
+        $this->description = $frontMatter['description'] ?? '';
+        $this->keywords = $frontMatter['keywords'] ?? '';
+        $this->pubDate = $frontMatter['pubDate'] ?? '';
+        $this->heroImage = $frontMatter['heroImage'] ?? '';
+        $this->category = $frontMatter['category'] ?? '';
     }
 }
