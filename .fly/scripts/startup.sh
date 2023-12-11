47ff58ea6fcccad90e23fdc17495e843f7d791ad
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-12-11 10:48:14 -0800

    chore: remove fly

--- .fly/scripts/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/php /var/www/html/artisan app:sync-content
-/usr/bin/php /var/www/html/artisan app:generate-sitemap
--- database/migrations/2023_12_11_044458_create_notes_table.php
+++ database/migrations/2023_12_11_044458_create_notes_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('content_syncs', function (Blueprint $table) {
+        Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->string('commit');
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('content_syncs');
+        Schema::dropIfExists('notes');
     }
 };
