e492588132a2d9e8d49a079498ddfa451c9f8b2e
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-12-11 16:30:24 -0800

    chore: remove non-compiling code... still not sure why

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,16 +1,22 @@
 import { createInertiaApp } from "@inertiajs/react";
 import createServer from "@inertiajs/react/server";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import * as React from "react";
 import * as ReactDOMServer from "react-dom/server";
 
-createServer((page) =>
-    createInertiaApp({
-        page,
-        render: ReactDOMServer.renderToString,
-        resolve: (name) => {
-            const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
-            return pages[`./Pages/${name}.jsx`];
-        },
-        setup: ({ App, props }) => <App {...props} />,
-    }),
+const appName = import.meta.env.VITE_APP_NAME ?? "joeymckenzie.tech";
+
+createServer(
+    async (page) =>
+        await createInertiaApp({
+            page,
+            render: ReactDOMServer.renderToString,
+            title: (title) => `${title} - ${appName}`,
+            resolve: async (name) =>
+                await resolvePageComponent(
+                    `./Pages/${name}.tsx`,
+                    import.meta.glob("./Pages/**/*.tsx"),
+                ),
+            setup: ({ App, props }) => <App {...props} />,
+        }),
 );
