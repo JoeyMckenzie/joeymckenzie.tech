8e25e377acafb0d601f7ee87169e3991dc634d12
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-12-13 14:49:26 -0800

    chore: wip

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -6,28 +6,27 @@ import * as ReactDOMServer from "react-dom/server";
 
 const appName = import.meta.env.VITE_APP_NAME ?? "joeymckenzie.tech";
 
-createServer(
-    async (page) =>
-        await createInertiaApp({
-            page,
-            render: ReactDOMServer.renderToString,
-            title: (title) => `${title} - ${appName}`,
-            resolve: async (name) =>
-                await resolvePageComponent(
-                    `./Pages/${name}.tsx`,
-                    import.meta.glob("./Pages/**/*.tsx"),
-                ),
-            setup: ({ App, props }) => {
-                // @ts-expect-error ziggy will be defined
-                global.route<RouteName> = (name, params, absolute) =>
-                    route(name, params, absolute, {
-                        // @ts-expect-error ziggy will be defined
-                        ...page.props.ziggy,
-                        // @ts-expect-error ziggy will be defined
-                        location: new URL(page.props.ziggy.location),
-                    });
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: ReactDOMServer.renderToString,
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.tsx`,
+                import.meta.glob("./Pages/**/*.tsx"),
+            ),
+        setup: ({ App, props }) => {
+            // @ts-expect-error ziggy will be defined
+            global.route<RouteName> = (name, params, absolute) =>
+                route(name, params, absolute, {
+                    // @ts-expect-error
+                    ...page.props.ziggy,
+                    // @ts-expect-error
+                    location: new URL(page.props.ziggy.location),
+                });
 
-                return <App {...props} />;
-            },
-        }),
+            return <App {...props} />;
+        },
+    }),
 );
