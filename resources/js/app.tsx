c2fefd4f9a190519853d62b8365976c60a4a857e
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-12-13 15:13:01 -0800

    chore: wip

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,19 +4,20 @@ import "./bootstrap";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import * as React from "react";
-import { hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 const appName = import.meta.env.VITE_APP_NAME ?? "joeymckenzie.tech";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: async (name) =>
-        await resolvePageComponent(
+    resolve: (name) =>
+        resolvePageComponent(
             `./Pages/${name}.tsx`,
             import.meta.glob("./Pages/**/*.tsx"),
         ),
     setup({ el, App, props }) {
-        hydrateRoot(el, <App {...props} />);
+        const root = createRoot(el);
+        root.render(<App {...props} />);
     },
     progress: {
         color: "#4B5563",
