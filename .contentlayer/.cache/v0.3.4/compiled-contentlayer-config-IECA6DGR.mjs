ee35c7e05f94390ddb487d969ef17165c7770742
Joey <joey.mckenzie27@gmail.com>
2024-01-02 12:28:44 -0800

    chore: add spotify

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-IECA6DGR.mjs
@@ -0,0 +1,42 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypePrettyCode from "rehype-pretty-code";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: "**/*.md",
+  contentType: "markdown",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    pubDate: { type: "date", required: true },
+    category: { type: "string", required: true },
+    heroImage: { type: "string", required: true },
+    draft: { type: "boolean", required: false, default: false },
+    keywords: { type: "list", of: { type: "string" }, required: true }
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/blog/${post._raw.flattenedPath}`
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "vitesse-dark"
+        }
+      ]
+    ]
+  }
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-IECA6DGR.mjs.map
---
+++ contentlayer.config.ts
@@ -0,0 +1,38 @@
+import { defineDocumentType, makeSource, } from 'contentlayer/source-files';
+import rehypePrettyCode from 'rehype-pretty-code';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: "**/*.md",
+  contentType: 'markdown',
+  fields: {
+    title: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    pubDate: { type: 'date', required: true },
+    category: { type: 'string', required: true },
+    heroImage: { type: 'string', required: true },
+    draft: { type: 'boolean', required: false, default: false },
+    keywords: { type: 'list', of: { type: 'string' },  required: true }
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (post) => `/blog/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: './content',
+  documentTypes: [Post],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: 'vitesse-dark',
+        },
+      ],
+    ],
+  },
+});
