149d32668fd9110ef6a42b0baf987d596378b83b
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-01-09 23:03:03 -0800

    chore: wip

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 // next.config.js
-const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-   swcMinify: true
-  }
+  swcMinify: true,
+};
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig);
