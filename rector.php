1fde3ee0239cf0d9b1421ca37ed82e41c982ce9a
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-03-03 21:32:53 -0800

    chore: add rector

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/tests',
+    ])
+    ->withPhpSets(php83: true)
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSets([
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+    ]);
