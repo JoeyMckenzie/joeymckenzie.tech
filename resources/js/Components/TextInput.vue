219c55dbf3629f542061a57ccebd9fa5d9c918ac
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-03-03 16:43:25 -0800

    Install Breeze

---
+++ resources/js/Components/TextInput.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const model = defineModel<string>({ required: true });
+
+const input = ref<HTMLInputElement | null>(null);
+
+onMounted(() => {
+    if (input.value?.hasAttribute('autofocus')) {
+        input.value?.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value?.focus() });
+</script>
+
+<template>
+    <input
+        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        v-model="model"
+        ref="input"
+    />
+</template>
