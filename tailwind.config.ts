e02979c13228f6654f60b6a76d21e5c06cd1f446
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-03-03 21:42:08 -0800

    chore: move config to TS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import { Config } from 'tailwindcss';
 
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -19,4 +19,4 @@ export default {
     },
 
     plugins: [forms],
-};
+} satisfies Config;
