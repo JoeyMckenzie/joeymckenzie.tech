3dbaae38d44a9e5e9eec8cb0fd68fc3761c48dc2
Joey <joey.mckenzie27@gmail.com>
2024-03-11 11:34:56 -0700

    chore: wip

--- app/Http/Controllers/ViewsController.php
+++ app/Http/Controllers/ViewsController.php
@@ -8,10 +8,13 @@ use App\Models\Post;
 
 final class ViewsController extends Controller
 {
-    public function __invoke()
+    /**
+     * @return array<int, array{slug: string, views: int}>
+     */
+    public function __invoke(): array
     {
         $posts = Post::query()->get(['slug', 'views']);
 
-        return $posts;
+        return $posts->toArray();
     }
 }
