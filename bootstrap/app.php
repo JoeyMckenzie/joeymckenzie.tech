504045008668e3afc8a5e6ce6b737ed042bf503a
Joey <joey.mckenzie27@gmail.com>
2024-03-12 13:19:24 -0700

    chore: wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(App\Http\Middleware\TrustProxies::class);
         $middleware->web(append: [
             App\Http\Middleware\HandleInertiaRequests::class,
             Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
