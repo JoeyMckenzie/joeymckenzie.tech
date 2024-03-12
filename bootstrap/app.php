792db3f1f32488ba1e9c0b213ee67ff5f827e591
Joey <joey.mckenzie27@gmail.com>
2024-03-12 15:14:47 -0700

    chore: wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -16,4 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             App\Http\Middleware\HandleInertiaRequests::class,
             Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
     })->create();
