c0a0b43a87bc470bb01816104718982d4955b6ae
Joey <joey.mckenzie27@gmail.com>
2024-03-12 13:44:39 -0700

    chore: wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,7 +22,12 @@
     @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
 </head>
+
 <body class="font-sans antialiased">
-@inertia
+    @inertia
+    <script>
+        Ziggy.url = '{{ env('APP_URL') }}'
+    </script>
 </body>
+
 </html>
