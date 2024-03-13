7df26a78354f1b1c682be4a5be5eca4bce2d6eaa
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-03-12 20:54:45 -0700

    chore: wip

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -5,7 +5,7 @@ import createServer from '@inertiajs/vue3/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const appName = import.meta.env.VITE_APP_NAME ?? 'joeymckenzie.tech';
+const appName = import.meta.env.VITE_APP_NAME;
 
 createServer((page) =>
     createInertiaApp({
