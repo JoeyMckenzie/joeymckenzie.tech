06d3188de52d35ae2488c32a4d19e75322c10cc3
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-04-11 22:44:59 -0700

    chore: wip

--- src/components/theme_toggle.rs
+++ src/components/theme_toggle.rs
@@ -1,78 +1,59 @@
 use gloo_storage::Storage;
 use leptos::*;
 
-const THEMES: [&str; 32] = [
-    "light",
-    "dark",
-    "cupcake",
-    "bumblebee",
-    "emerald",
-    "corporate",
-    "synthwave",
-    "retro",
-    "cyberpunk",
-    "valentine",
-    "halloween",
-    "garden",
-    "forest",
-    "aqua",
-    "lofi",
-    "pastel",
-    "fantasy",
-    "wireframe",
-    "black",
-    "luxury",
-    "dracula",
-    "cmyk",
-    "autumn",
-    "business",
-    "acid",
-    "lemonade",
-    "night",
-    "coffee",
-    "winter",
-    "dim",
-    "nord",
-    "sunset",
-];
-
 #[component]
 pub fn ThemeToggle() -> impl IntoView {
     let (theme, set_theme) = create_signal("light");
     let html_element: NodeRef<html::Html> = create_node_ref();
+    let themes = create_rw_signal(vec![
+        "light",
+        "dark",
+        "cupcake",
+        "bumblebee",
+        "emerald",
+        "corporate",
+        "synthwave",
+        "retro",
+        "cyberpunk",
+        "valentine",
+        "halloween",
+        "garden",
+        "forest",
+        "aqua",
+        "lofi",
+        "pastel",
+        "fantasy",
+        "wireframe",
+        "black",
+        "luxury",
+        "dracula",
+        "cmyk",
+        "autumn",
+        "business",
+        "acid",
+        "lemonade",
+        "night",
+        "coffee",
+        "winter",
+        "dim",
+        "nord",
+        "sunset",
+    ]);
 
     create_effect(move |_| {
         let storage = gloo_storage::LocalStorage::raw();
         logging::log!("{storage:?}");
     });
 
-    create_effect(move |_| {
-        logging::log!("{:?}", html_element.get().unwrap().get_attribute_names());
-    });
+    // create_effect(move |_| {
+    //     logging::log!("{:?}", html_element.get().unwrap().get_attribute_names());
+    // });
 
     view! {
-        <label class="swap swap-rotate">
-            // <!-- this hidden checkbox controls the state -->
-            <input
-                type="checkbox"
-                class="theme-controller"
-                value=theme.get()
-                on:click=move |_| {
-                    if theme.get() == "forest" {
-                        set_theme("light");
-                    } else {
-                        set_theme("forest");
-                    }
-                }
-            />
-
-            // <!-- sun icon -->
-            <Show
-                when=move || { theme.get() == "forest" }
-                fallback=|| view! { <span class="w-6 h-6 icon-[solar--moon-broken]"></span> }
-            >
-                <span class="w-6 h-6 icon-[solar--sun-broken]"></span>
-            </Show>
-        </label>
+        <select class="select w-full max-w-xs">
+            <For each=themes key=|theme| theme.to_string() let:theme>
+                <option>{theme}</option>
+            </For>
+        </select>
     }
 }
