4478ff5085c45a10a9410815956e73cd2b30c085
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-04-14 22:25:17 -0700

    chore: wip

--- Justfile
+++ Justfile
@@ -5,6 +5,9 @@ default: dev
 dev:
     just watch & find src/ | entr -s 'just fmt && npm run tailwind:compile'
 
+build:
+    npm run tailwind:compile && just content && just sitemap && cargo leptos build --release -vv
+
 watch:
     cargo leptos watch
 
--- src/components/theme_toggle.rs
+++ src/components/theme_toggle.rs
@@ -1,52 +1,41 @@
-use gloo_storage::Storage;
 use leptos::*;
 use leptos_meta::*;
+use leptos_use::{use_color_mode_with_options, ColorMode, UseColorModeOptions, UseColorModeReturn};
 
-const THEME_KEY: &str = "joeymckenzie.tech-theme";
 const DARK_THEME: &str = "forest";
 const LIGHT_THEME: &str = "light";
 
 #[component]
 pub fn ThemeToggle() -> impl IntoView {
-    let (theme, set_theme) = create_signal(LIGHT_THEME);
+    let UseColorModeReturn { mode, set_mode, .. } =
+        use_color_mode_with_options(UseColorModeOptions::default().cookie_enabled(true));
 
-    create_effect(move |_| {
-        match gloo_storage::LocalStorage::get::<String>("joeymckenzie.tech-theme") {
-            Ok(stored_theme) => {
-                if stored_theme == DARK_THEME {
-                    set_theme(DARK_THEME);
-                } else {
-                    set_theme(LIGHT_THEME);
-                }
-            }
-            Err(_) => logging::warn!("theme not found"),
-        }
-    });
+    create_effect(move |_| logging::log!("mode {:?}", mode()));
 
     view! {
-        <Html attr:data-theme=move || theme.get()/>
+        <Html attr:data-theme=move || match mode.get() {
+            ColorMode::Light => LIGHT_THEME,
+            ColorMode::Dark => DARK_THEME,
+            _ => DARK_THEME,
+        }/>
         <label class="swap swap-rotate">
             <input
                 type="checkbox"
                 class="theme-controller"
-                value=move || theme.get()
-                on:click=move |_| {
-                    if theme.get() == DARK_THEME {
-                        set_theme(LIGHT_THEME);
-                        let _ = gloo_storage::LocalStorage::set(THEME_KEY, LIGHT_THEME);
-                    } else {
-                        set_theme(DARK_THEME);
-                        let _ = gloo_storage::LocalStorage::set(THEME_KEY, DARK_THEME);
-                    }
+                on:click=move |_| match mode() {
+                    ColorMode::Light => set_mode(ColorMode::Dark),
+                    ColorMode::Dark => set_mode(ColorMode::Light),
+                    _ => logging::warn!("color mode invalid"),
                 }
             />
 
-            <Show
-                when=move || theme.get() == LIGHT_THEME
-                fallback=|| view! { <span class="w-5 h-5 icon-[pixelarticons--sun]"></span> }
-            >
-                <span class="w-5 h-5 icon-[pixelarticons--moon]"></span>
-            </Show>
+            {move || match mode.get() {
+                ColorMode::Light => {
+                    view! { <span class="w-5 h-5 icon-[pixelarticons--moon]"></span> }
+                }
+                ColorMode::Dark => view! { <span class="w-5 h-5 icon-[pixelarticons--sun]"></span> },
+                _ => view! { <span></span> },
+            }}
 
         </label>
     }
