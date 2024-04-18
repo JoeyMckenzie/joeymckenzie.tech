afb2dbcc46b662aa3511a450ad95bf1293b97802
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-04-17 23:07:37 -0700

    chore: wip

--- src/components/theme_toggle.rs
+++ src/components/theme_toggle.rs
@@ -8,16 +8,30 @@ const LIGHT_THEME: &str = "light";
 
 #[server(ToggleTheme, "/api/mode/toggle")]
 pub async fn toggle_theme() -> Result<(), ServerFnError> {
-    use http::{header, HeaderMap, HeaderValue, StatusCode};
+    use axum_extra::extract::CookieJar;
+    use http::{header, HeaderMap, HeaderValue};
     use leptos_axum::{extract, ResponseOptions, ResponseParts};
 
-    let response =
-        use_context::<ResponseOptions>().expect("to have leptos_actix::ResponseOptions provided");
+    let cookies: CookieJar = extract().await?;
+    let mut updated_theme = LIGHT_THEME;
+
+    if let Some(theme) = cookies.get(COOKIE_NAME) {
+        updated_theme = if theme.value().eq(LIGHT_THEME) {
+            DARK_THEME
+        } else {
+            LIGHT_THEME
+        };
+    }
+
+    let response = expect_context::<ResponseOptions>();
     let mut response_parts = ResponseParts::default();
     let mut headers = HeaderMap::new();
     headers.insert(
         header::SET_COOKIE,
-        HeaderValue::from_str("darkmode=true; Path=/").expect("to create header value"),
+        HeaderValue::from_str(&format!(
+            "{}={}; Path=/; SameSite=Lax",
+            COOKIE_NAME, updated_theme,
+        ))?,
     );
     response_parts.headers = headers;
     response.overwrite(response_parts);
@@ -25,24 +39,43 @@ pub async fn toggle_theme() -> Result<(), ServerFnError> {
     Ok(())
 }
 
-fn get_initial_theme() -> String {
-    use leptos_use::{use_cookie, utils::FromToStringCodec};
+#[server(GetCurrentTheme, "/api/mode")]
+pub async fn get_current_theme() -> Result<String, ServerFnError> {
+    use axum_extra::extract::CookieJar;
+    use leptos_axum::extract;
 
-    let (counter, set_counter) = use_cookie::<u32, FromToStringCodec>("counter");
+    let cookies: CookieJar = extract().await?;
 
-    LIGHT_THEME.to_string()
+    if let Some(theme) = cookies.get(COOKIE_NAME) {
+        dbg!(&theme);
+        return Ok(theme.value().to_string());
+    }
+
+    Ok(LIGHT_THEME.to_string())
 }
 
 #[component]
 pub fn ThemeToggle() -> impl IntoView {
+    let initial_mode = create_resource(|| (), move |_| get_current_theme());
     let toggle_theme = create_server_action::<ToggleTheme>();
 
     view! {
-        <Html attr:data-theme=LIGHT_THEME/>
-        <ActionForm action=toggle_theme>
-            <button type="submit" class="flex items-center">
-                <span class="w-5 h-5 swap-off fill-current icon-[pixelarticons--moon]"></span>
-            </button>
-        </ActionForm>
+        <Suspense>
+            {move || {
+                if let Some(theme) = initial_mode.get() {
+                    view! {
+                        <Html attr:data-theme=LIGHT_THEME/>
+                        <ActionForm action=toggle_theme>
+                            <button type="submit" class="flex items-center">
+                                <span class="w-5 h-5 swap-off fill-current icon-[pixelarticons--moon]"></span>
+                            </button>
+                        </ActionForm>
+                    }
+                } else {
+                    view! { <span></span> }.into_any()
+                }
+            }}
+
+        </Suspense>
     }
 }
