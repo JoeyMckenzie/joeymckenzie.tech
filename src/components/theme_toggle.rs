a0c7628a2bc9889ba2bb0680634137fa59716616
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-04-17 23:16:03 -0700

    chore: wip

--- src/components/theme_toggle.rs
+++ src/components/theme_toggle.rs
@@ -7,7 +7,7 @@ const DARK_THEME: &str = "forest";
 const LIGHT_THEME: &str = "light";
 
 #[server(ToggleTheme, "/api/mode/toggle")]
-pub async fn toggle_theme() -> Result<(), ServerFnError> {
+pub async fn toggle_theme() -> Result<String, ServerFnError> {
     use axum_extra::extract::CookieJar;
     use http::{header, HeaderMap, HeaderValue};
     use leptos_axum::{extract, ResponseOptions, ResponseParts};
@@ -36,7 +36,7 @@ pub async fn toggle_theme() -> Result<(), ServerFnError> {
     response_parts.headers = headers;
     response.overwrite(response_parts);
 
-    Ok(())
+    Ok(updated_theme.to_string())
 }
 
 #[server(GetCurrentTheme, "/api/mode")]
@@ -59,23 +59,35 @@ pub fn ThemeToggle() -> impl IntoView {
     let initial_mode = create_resource(|| (), move |_| get_current_theme());
     let toggle_theme = create_server_action::<ToggleTheme>();
 
+    create_effect(move |_| {
+        logging::log!("{:?}", toggle_theme.value().get());
+    });
+
     view! {
         <Suspense>
-            {move || {
-                if let Some(theme) = initial_mode.get() {
-                    view! {
-                        <Html attr:data-theme=LIGHT_THEME/>
-                        <ActionForm action=toggle_theme>
-                            <button type="submit" class="flex items-center">
-                                <span class="w-5 h-5 swap-off fill-current icon-[pixelarticons--moon]"></span>
-                            </button>
-                        </ActionForm>
-                    }
-                } else {
-                    view! { <span></span> }.into_any()
-                }
-            }}
+            <Html attr:data-theme=move || match initial_mode.get() {
+                Some(theme) => theme.unwrap_or(LIGHT_THEME.to_string()),
+                None => LIGHT_THEME.to_string(),
+            }/>
+            <ActionForm action=toggle_theme>
+                <button type="submit" class="flex items-center">
+                    <Show
+                        when=move || match toggle_theme.value().get() {
+                            Some(theme) => theme.unwrap_or(LIGHT_THEME.to_string()).eq(LIGHT_THEME),
+                            None => false,
+                        }
 
+                        fallback=|| {
+                            view! {
+                                <span class="w-5 h-5 fill-current icon-[pixelarticons--sun]"></span>
+                            }
+                        }
+                    >
+
+                        <span class="w-5 h-5 fill-current icon-[pixelarticons--moon]"></span>
+                    </Show>
+                </button>
+            </ActionForm>
         </Suspense>
     }
 }
