7bfe56132679e795bd53588c4452a3a7d7a2ae58
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-05-08 16:04:18 -0700

    chore: wip

--- app/Livewire/Pages/BlogPost.php
+++ app/Livewire/Pages/BlogPost.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Livewire\Pages;
 
 use App\Models\Post;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -12,7 +13,7 @@ final class BlogPost extends Component
 {
     public Post $post;
 
-    public function mount(string $slug): void
+    public function mount(string $slug, Request $request): void
     {
         $post = Post::query()
             ->with('keywords')
@@ -31,8 +32,26 @@ final class BlogPost extends Component
             abort(404);
         }
 
-        $post->views += 1;
-        $post->save();
+        // We only want legit views from users, not bots so check the
+        // user-agent to see if it contains a known bot crawler
+        $userAgent = $request->headers->get('user-agent') ?? '';
+        $botPatterns = [
+            '/googlebot/i', // Google's web crawler
+            '/bingbot/i',   // Microsoft's Bing bot
+            '/slurp/i',     // Yahoo's search engine bot
+            '/duckduckbot/i', // DuckDuckGo's bot
+            '/baiduspider/i', // Baidu's bot
+            '/yandexbot/i', // Yandex's bot
+        ];
+
+        $matchedUserAgentBot = collect($botPatterns)->first(fn (string $pattern): bool => preg_match($pattern, $userAgent) === 1);
+
+        // If a bot was detected, we won't increment the view count - I mean come on, I'm not that _that_ popular...
+        if ($matchedUserAgentBot === null) {
+            $post->views += 1;
+            $post->save();
+        }
+
         $this->post = $post;
     }
 
