dba3ce51b06091a777339be9e4f70f1f3c2febd9
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-08-09 14:50:42 -0700

    chore: wip

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="mx-auto mt-auto flex w-full flex-col items-center justify-between gap-y-8 p-12 sm:flex-row sm:items-end"
+    class="mx-auto mt-auto flex flex-col items-center justify-between gap-y-12 px-4 sm:flex-row sm:items-end"
 >
     <livewire:components.spotify-currently-listening />
     <x-powered-by />
