270b7731d62e2b94689a8cf188cf08f2a5e3311a
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-08-09 12:23:45 -0700

    chore: wip

--- app.vue
+++ app.vue
@@ -7,6 +7,12 @@ await fetchPosts();
 </script>
 
 <template>
+  <Head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+  </Head>
   <Body class="mx-auto max-w-3xl p-4 sm:px-6 lg:px-8">
     <NuxtLayout>
       <Navbar />
--- pages/now.vue
+++ pages/now.vue
@@ -1,4 +1,3 @@
-import { ULink } from '#build/components';
 <template>
   <main class="py-12">
     <h2 class="text-2xl font-bold tracking-tight">
@@ -15,28 +14,19 @@ import { ULink } from '#build/components';
         <li>
           I'm part of team modernizing financial systems for a lending
           company with things like
-          <a href="https://dotnet.microsoft.com/en-us/">.NET</a>
-          ,
-          <a href="https://aws.amazon.com/">AWS,</a>
-          <a href="https://www.docker.com/">Docker,</a>
-          <a href="https://www.terraform.io/">Terraform</a>
-          , and a bunch of other resume buzzwords. I like breaking
+          <a href="https://dotnet.microsoft.com/en-us/">.NET</a>,
+          <a href="https://aws.amazon.com/">AWS</a>,
+          <a href="https://www.docker.com/">Docker</a>,
+          <a href="https://www.terraform.io/">Terraform</a>, and a bunch of other resume buzzwords. I like breaking
           things and convincing people that you're legally allowed to
           deploy .NET to places other than Azure.
         </li>
         <li>
           I have a secret love for PHP and Laravel and have written a
-          few things about them on my
-          <a href="/blog">blog</a>
-          . I've also published a
-          <a href="https://packagist.org/packages/joeymckenzie/openbrewerydb-php-api">
-            package or two
-          </a>
-          . I'm even working on a
+          few things about them on my <a href="/blog">blog</a>. I've also published a
+          <a href="https://packagist.org/packages/joeymckenzie/openbrewerydb-php-api">package or two</a>. I'm even working on a
           <a href="https://hopjobs.xyz">job board</a>
-          for breweries. Even the website you're reading is powered by
-          <a href="https://livewire.laravel.com">Livewire!</a>
-          If you're hiring .NET-to-PHP-converts... I know a guy.
+          for breweries. If you're hiring .NET-to-PHP-converts... I know a guy.
         </li>
         <li>
           I write a lot of
