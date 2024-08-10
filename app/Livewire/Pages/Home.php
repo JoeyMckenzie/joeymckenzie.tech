4dbbe3d58df87ca7b4be506f30164fbb1e05f7a4
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-08-09 21:01:23 -0700

    chore: wip

--- app/Livewire/Pages/Home.php
+++ app/Livewire/Pages/Home.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Livewire\Pages;
 
+use App\Models\Post;
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -11,6 +13,24 @@ use Livewire\Component;
 #[Title("Hey, I'm Joey.")]
 final class Home extends Component
 {
+    /** @var Collection<int, Post> */
+    public ?Collection $posts = null;
+
+    public function mount(): void
+    {
+        $this->posts = Post::select([
+            'slug',
+            'published_date',
+            'category',
+            'description',
+            'title',
+            'views',
+        ])
+            ->orderByDesc('published_date')
+            ->limit(3)
+            ->get();
+    }
+
     public function render(): View
     {
         return view('livewire.pages.index');
--- resources/views/livewire/pages/index.blade.php
+++ resources/views/livewire/pages/index.blade.php
@@ -1,15 +1,24 @@
-<div class="mx-auto flex flex-col items-center py-12">
-    <div class="prose dark:prose-invert">
-        <h2 class="text-4xl font-extrabold tracking-tight sm:text-4xl">
-            Hi, I'm Joey.
-        </h2>
-        <p class="mx-auto mt-6">
-            I'm a software developer based in Northern California working in
-            fintech. I enjoy writing about software, design, dad jokes, and
-            cheap beer among a few other things. I like building fast, efficient
-            web services, learning new things, and writing code in the open
-            source ecosystem.
-        </p>
+<div class="mx-auto flex flex-col items-center space-y-12 py-12">
+    <div>
+        <div class="prose dark:prose-invert">
+            <h2 class="text-4xl font-extrabold tracking-tight sm:text-4xl">
+                Hi, I'm Joey.
+            </h2>
+            <p class="mx-auto mt-6">
+                I'm a software developer based in Northern California working in
+                fintech. I enjoy writing about software, design, dad jokes, and
+                cheap beer among a few other things. I like building fast,
+                efficient web services, learning new things, and writing code in
+                the open source ecosystem.
+            </p>
+        </div>
+        <x-social-buttons />
     </div>
-    <x-social-buttons />
+    <x-blog-previews :posts="$posts" />
+    <a href="{{ route("blog") }}">
+        <x-button lime>
+            Blog
+            <span class="icon-[gridicons--external] h-5 w-5"></span>
+        </x-button>
+    </a>
 </div>
