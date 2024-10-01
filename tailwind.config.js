ed2cde0990e476dfe9caae29394fb48cf95e8513
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-10-01 11:38:29 -0700

    chore: wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+import daisy from 'daisyui'
 import preset from './vendor/filament/support/tailwind.config.preset'
 
+/** @type {import('tailwindcss').Config} */
 export default {
-    presets: [preset],
-    content: [
-        './app/Filament/**/*.php',
-        './resources/views/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
-    ],
+  presets: [preset],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './vendor/filament/**/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  daisyui: {
+    themes: ['dim'],
+  },
+  plugins: [forms, typography, daisy],
 }
