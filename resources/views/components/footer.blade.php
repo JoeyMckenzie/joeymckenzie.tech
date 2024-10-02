77a79319b223b3fab3b7cf4b5c6e84bfb7ddd580
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-10-02 13:40:49 -0700

    chore: wip

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div
     class="max-w-screen-4xl mx-auto mt-auto flex w-full flex-col items-center justify-evenly gap-y-8 p-12 sm:flex-row sm:items-end">
     <x-spotify-currently-listening>
-        <x-bi-spotify class="size-6 text-green-500" />
+        <x-bi-spotify class="size-6 text-green-600" />
     </x-spotify-currently-listening>
     <x-powered-by />
 </div>
