88fa84ff978e04b6db0344a6a15e7d0834cf8a14
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-10-28 13:13:57 -0700

    chore: wip

--- app/Models/Keyword.php
+++ app/Models/Keyword.php
@@ -7,24 +7,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-/**
- * @property int $id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string $word
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Post> $posts
- * @property-read int|null $posts_count
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Keyword newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Keyword newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Keyword query()
- * @method static \Illuminate\Database\Eloquent\Builder|Keyword whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Keyword whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Keyword whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Keyword whereWord($value)
- *
- * @mixin \Eloquent
- */
 final class Keyword extends Model
 {
     protected $fillable = [
@@ -36,7 +18,7 @@ final class Keyword extends Model
     ];
 
     /**
-     * @return BelongsToMany<Post>
+     * @return BelongsToMany<Post, covariant $this>
      */
     public function posts(): BelongsToMany
     {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,42 +9,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Number;
 
-/**
- * @property int $id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string $title
- * @property string $description
- * @property string $slug
- * @property \Illuminate\Support\Carbon $published_date
- * @property string $hero_image
- * @property string $category
- * @property int $views
- * @property string $raw_content
- * @property string $parsed_content
- * @property-read string $display_date
- * @property-read string $formatted_views
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Keyword> $keywords
- * @property-read int|null $keywords_count
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Post query()
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereCategory($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereHeroImage($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereParsedContent($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post wherePublishedDate($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereRawContent($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereViews($value)
- *
- * @mixin \Eloquent
- */
 final class Post extends Model
 {
     protected $fillable = [
@@ -83,7 +47,7 @@ final class Post extends Model
     ];
 
     /**
-     * @return BelongsToMany<Keyword>
+     * @return BelongsToMany<Keyword, covariant $this>
      */
     public function keywords(): BelongsToMany
     {
