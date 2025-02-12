06ce17b287e8ae6cd91c683df9e45b18e0cdcc76
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-02-12 11:40:19 -0800

    chore: wip

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,10 +1,12 @@
 <x-layout title="Blog." subtitle="Adventures in code. Legacy nightmares. Bright shiny new frameworks.">
     <div style="padding-top: 2rem;">
         @foreach ($posts as $post)
+            <small>{{ $post->display_date }}</small>
             <div>
                 <a href="{{ route('blog.show', ['slug' => $post->slug]) }}">{{ $post->title }}</a>
-                <p>{{  $post->description }}</p>
+                <p>{{ $post->description }}</p>
             </div>
+            <hr/>
         @endforeach
     </div>
 </x-layout>
