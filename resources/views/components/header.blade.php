ded12d0e09d026dffd0299d5d0a174a6fddc1b6b
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-02-14 20:09:04 -0800

    chore: wip

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,18 +1,14 @@
-@props([
-    'title',
-    'subtitle',
-    'extra' => null
-])
+@props(['title', 'subtitle', 'extra' => null])
 
 <header>
     <nav>
         <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'current' : null }}">Home</a>
         <a href="{{ route('now') }}" class="{{ request()->routeIs('now') ? 'current' : null }}">Now</a>
         <a href="{{ route('blog.index') }}"
-           class="{{ request()->routeIs('blog.index') || request()->routeIs('blog.show') ? 'current' : null }}">Blog</a>
-        <h3>{{ $title }}</h3>
+            class="{{ request()->routeIs('blog.index') || request()->routeIs('blog.show') ? 'current' : null }}">Blog</a>
+        <h3 style="width: 50%; margin-left: auto; margin-right: auto; text-align: center">{{ $title }}</h3>
         <p>{{ $subtitle }}</p>
-        @if($extra !== null)
+        @if ($extra !== null)
             <p>{{ $extra }}</p>
         @endif
     </nav>
