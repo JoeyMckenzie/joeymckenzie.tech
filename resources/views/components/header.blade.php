ddfcc9000dba2a50e657bf05d7ff62ca5cd93aa0
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-02-18 22:13:03 -0800

    chore: wip

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,9 +2,10 @@
 
 <header>
     <nav>
-        <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'current' : null }}">Home</a>
-        <a href="{{ route('now') }}" class="{{ request()->routeIs('now') ? 'current' : null }}">Now</a>
-        <a href="{{ route('blog.index') }}"
+        <a data-pan="nav-home" href="{{ route('home') }}"
+            class="{{ request()->routeIs('home') ? 'current' : null }}">Home</a>
+        <a data-pan="nav-now" href="{{ route('now') }}" class="{{ request()->routeIs('now') ? 'current' : null }}">Now</a>
+        <a data-pan="nav-blog" href="{{ route('blog.index') }}"
             class="{{ request()->routeIs('blog.index') || request()->routeIs('blog.show') ? 'current' : null }}">Blog</a>
         <h3 style="max-width: 40ch; margin-left: auto; margin-right: auto; text-align: center">{{ $title }}</h3>
         <p>{{ $subtitle }}</p>
