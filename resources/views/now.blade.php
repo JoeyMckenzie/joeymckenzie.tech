c3b6ec090645b28e8eb2ee71bb3a7faa79afee25
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-03-04 14:36:45 -0800

    chore: wip

--- resources/views/now.blade.php
+++ resources/views/now.blade.php
@@ -10,6 +10,11 @@
         The goal is to make server management with Hetzner a breeze with PHP and learn a few things along the way.
     </p>
     <p>
+        I'm also working on a <a href="https://github.com/joeymckenzie/nasastan">PHPStan extension</a> to bring NASA's
+        <a href="https://en.wikipedia.org/wiki/The_Power_of_10:_Rules_for_Developing_Safety-Critical_Code">Power of
+            Ten</a> rules into your PHP code. Contributors welcome!
+    </p>
+    <p>
         I run a webdev <a href="https://doghead.digital">agency</a> with my amazing wife. Together, we build websites
         for small businesses with PHP, Laravel, and <a href="https://statamic.com/">Statamic</a>.
     </p>
