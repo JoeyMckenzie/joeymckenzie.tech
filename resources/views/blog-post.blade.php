5ddc694bef58587d4e049464fac8c3520f04670a
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-03-11 12:04:15 -0700

    chore: wip

--- resources/views/blog-post.blade.php
+++ resources/views/blog-post.blade.php
@@ -1,7 +1,7 @@
 <x-layout title="{!! $post->title !!}" :subtitle="$post->display_date">
     <div id="blog-content" data-pan="{{ sprintf('blog-post-%s', $post->slug) }}" style="padding-top: 2rem;">
         <img style="margin-left: auto; margin-right: auto; width: 75%; display: block;" src="{{ $post->hero_image }}"
-            alt="blog meme" />
+            height="400" width="400" alt="blog meme" />
         {!! $post->parsed_content !!}
     </div>
 </x-layout>
