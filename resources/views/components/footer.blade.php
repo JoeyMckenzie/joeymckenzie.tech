8451e070f4ae13f52bbdece6bc81acd6ec8c8b61
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-11-27 14:16:27 -0800

    chore: wip

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="mx-auto py-6 flex flex-row justify-between items-center gap-6 max-w-3xl px-4">
+<footer class="mx-auto py-6 flex flex-row justify-between items-baseline gap-6 max-w-3xl px-4">
     <livewire:components.spotify-player />
     <x-powered-by />
 </footer>
