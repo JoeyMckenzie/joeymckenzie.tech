10bc6a3920a4616c51df0d430bb47d10817a9356
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-11-27 13:38:28 -0800

    chore: wip

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $title ?? config('app.name') }}</title>
+    <title>{{ $title }} | {{ config_path('app.name') }}</title>
 
     <link rel="icon" href="/favicon.ico" sizes="any">
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
