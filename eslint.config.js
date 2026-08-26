b0f2f6bcc70ce535b8029989dbcd86872b9bbab1
Joey McKenzie <joey.mckenzie27@gmail.com>
2026-08-25 22:54:34 -0700

    feat: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js"
+import { defineConfig, globalIgnores } from "eslint/config"
+import astro from "eslint-plugin-astro"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+    globalIgnores(["dist/", ".astro/", ".devenv/", ".direnv/"]),
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    astro.configs["flat/recommended"],
+    {
+        languageOptions: {
+            globals: { ...globals.browser, ...globals.node },
+        },
+    },
+])
